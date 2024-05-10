6368e7499854f71c3cb55ab025de8e357463a45a
Adel-2003 <136308194+Adel-2003@users.noreply.github.com>
2024-05-10 14:39:37 +0300

    Add files via upload

---
+++ Proxy1/src/Db/DataAccessException.java
@@ -0,0 +1,10 @@
+
+public class DataAccessException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+	
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ Proxy1/src/java/BookDao.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface BookDao {
+	List<Book> getAllBooks() throws Exception;
+}
